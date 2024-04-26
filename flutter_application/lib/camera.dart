6b1cb37e5287a2354736e9d50d75a22b731a2722
rrreb <kshoah@connect.ust.hk>
2024-04-27 00:01:53 +0800

    added swiper

--- flutter_application/lib/camera.dart
+++ flutter_application/lib/camera.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -12,11 +13,16 @@ class CameraPage extends StatefulWidget {
   CameraPageState createState() => CameraPageState();
 }
 
+enum features {LOGO, TEXTURE}
+
 class CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
   CameraController? _controller;
   bool _isCameraInitialized = false;
   late final List<CameraDescription> _cameras;
   bool _isRecording = false;
+
+  features feature = features.LOGO;
+  List<bool> hasPhoto = [false, false];
 
   @override
   void initState() {
@@ -89,19 +95,21 @@ class CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
 
   Future<XFile?> capturePhoto() async {
     final CameraController? cameraController = _controller;
-    if (cameraController!.value.isTakingPicture) {
-      // A capture is already pending, do nothing.
-      return null;
-    }
+    // if (cameraController!.value.isTakingPicture) {
+    //   // A capture is already pending, do nothing.
+    //   return null;
+    // }
     try {
-      await cameraController.setFlashMode(FlashMode.off); //optional
+      await cameraController!.setFlashMode(FlashMode.off); //optional
       XFile file = await cameraController.takePicture();
-      return file;
+      // return file;
+      return null;
     } on CameraException catch (e) {
       debugPrint('Error occured while taking picture: $e');
       return null;
     }
   }
+
 
   void _onTakePhotoPressed() async {
     final navigator = Navigator.of(context);
@@ -119,13 +127,76 @@ class CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
     }
   }
 
+
+
+  List<String> featuresList = ['Logo', 'Feature','Logo', 'Feature','Logo', 'Feature'];
+
+  late final logoImage;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final deviceRatio = size.width / size.height;
+
     if (_isCameraInitialized) {
       return SafeArea(
         child: Scaffold(
+          appBar: AppBar(
+              title: feature == features.LOGO
+                      ? Text('Logo')
+                      : Text('Texture'),
+          ),
           body: Column(children: [
-            CameraPreview(_controller!),
+            if (feature == features.LOGO)
+              Expanded(
+                child:Container(
+                  width: size.width,
+                  //   height: 700,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Container(
+                        width: size.width,
+                        height: size.width * _controller!.value.aspectRatio,
+                        child: Transform.rotate(
+                          angle: -90 * 3.1415926535897932 / 180,
+                          child: CameraPreview(_controller!),
+                        ), // this is my CameraPreview
+                      ),
+                    ),
+                ),
+              )
+            else
+             Text('testing'),
+            Text((feature == features.LOGO)? "Place the logo inside the box" : ''),
+            Container(
+              child: Swiper(
+                loop: false,
+                itemCount: featuresList.length,
+                itemWidth: MediaQuery.of(context).size.width/5,
+                viewportFraction: 1,
+                scale: 0.5,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        featuresList[index],
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  );
+                },
+                onIndexChanged: (int index) {
+                  setState(() {
+                    feature = features.values[index];
+                  });
+                },
+            ),
+              height: 54,
+            ),
             ElevatedButton(
               onPressed: _onTakePhotoPressed,
               style: ElevatedButton.styleFrom(
